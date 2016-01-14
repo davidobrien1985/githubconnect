56412f7a4707dc3c10c101573a49ddc048f1db5a
Ryan Yates <ryan.yates@kilasuit.org>
2016-01-14 04:22:46 +0000

    Removed unneded Line that forced Pester Tests to Run when Module is imported

--- GithubConnect/GithubConnect.psm1
+++ GithubConnect/GithubConnect.psm1
@@ -1,4 +1,3 @@
 $Scriptroot = Split-Path $script:MyInvocation.MyCommand.Path
 
-Get-ChildItem $Scriptroot\Public *.ps1 -Recurse | ForEach-Object { Import-Module $_.FullName }
-. $Scriptroot\*.Tests.ps1 #This is only in still as this was how I found the Module running - Advise that we remove this line on v1 release
+Get-ChildItem $Scriptroot\Public *.ps1 -Recurse | ForEach-Object { Import-Module $_.FullName } 
